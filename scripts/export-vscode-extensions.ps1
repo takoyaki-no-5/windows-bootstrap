2456a5a18011a6fffa0d71ce72c9243a556f575b
takoyaki4th <kamatagenta@gmail.com>
2026-06-07 22:59:00 +0900

    extensions.txtが更新されるかテスト

--- scripts/export-vscode-extensions.ps1
+++ scripts/export-vscode-extensions.ps1
@@ -1,5 +1,4 @@
-@'
-$ErrorActionPreference = "Stop"
+﻿$ErrorActionPreference = "Stop"
 
 $repo = Split-Path $PSScriptRoot -Parent
 $extensionsPath = Join-Path $repo "vscode\extensions.txt"
@@ -7,4 +6,3 @@ $extensionsPath = Join-Path $repo "vscode\extensions.txt"
 New-Item -ItemType Directory -Force (Split-Path $extensionsPath) | Out-Null
 
 code --list-extensions | Sort-Object | Set-Content -Encoding UTF8 $extensionsPath
-'@ | Set-Content -Encoding UTF8 .\scripts\export-vscode.ps1
